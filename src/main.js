d54f6d164e0e346837f4fee843da7ebb309838ba
Markrama143 <ramamarkvincent2@gmail.com>
2025-11-19 16:28:08 +0800

    Add axios instance to global properties in Vue app
    Made axios instance globally available in Vue app.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import axiosInstance from './api/interceptor'
 
-createApp(App).mount('#app')
+const app = createApp(App)
+
+// Make axios globally available
+app.config.globalProperties.$http = axiosInstance
+app.config.globalProperties.$axios = axiosInstance
+
+app.mount('#app')
